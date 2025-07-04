fbbcc8942f2773d9b5f9da299cd2f9151d909067
tina <llolandrade@gmail.com>
2025-07-04 17:36:04 -0300

    condiçao p1

---
+++ javascript/ex008.js
@@ -0,0 +1,6 @@
+var vel = 99
+console.log(`a velocidade do seu carro é ${vel} km/h`);
+if ( vel > 60) {
+    console.log(`voce ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`);
---
+++ javascript/ex009.js
@@ -0,0 +1,8 @@
+var pais = 'frança' 
+console.log(`vivendo em ${pais}`)
+if (pais == 'BRASIL'){
+    console.log('voce e Brasileiro!')
+}else{
+    console.log('voce e Etrangeiro!')
+}
+8
